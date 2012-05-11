14b4e503cbaca59d21cca57db4e50e08ef0ec241
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2012-05-11 11:34:25 +0200

    keres.py további bővítések

--- regexp/keres.py
+++ regexp/keres.py
@@ -1,13 +1,54 @@
-# encoding: utf-8
-# ipython-nal és ipython3-mal is működik %ed fájlnévvel behívható
+#!/usr/bin/env python
+# coding: utf-8
+"""
+
+Lefuttatva
+-------------
+
+Lefuttatás előtt az utolsó három sort érdemes változtatgatni, vigyázva, hogy a
+keres kezdete beljebb legyen húzva az if kezdetű sornál. A lefuttatásnak két
+módja szerepel alább.
+
+1. Elindítható parancssorból::
+
+    python keres.py
+
+2. Megnyitható IDLE-ből (vagy IDLE3-ból) és az F5 gombbal elindítható.
+
+Parancsértelmezőből
+---------------------
+
+ipython-nal és ipython3-mal vagy a hivatalos python parancsértelmezővel is
+működik, és behívható így, ha abban a könyvtárban indítottuk az ipythont, ahol
+a keres.py fájl van, vagy benne van az elérési útvonalban::
+
+    from keres import keres
+
+majd használható így::
+
+    keres(
+      r"^ http s? :// ([a-z-]+\.)+ [a-z]{2,3} (/[a-z-_\.]+)* $",
+      urls )
+
+A keres függvény pontos használatát megtudhatjuk az alábbi paranccsal::
+
+    help(keres)
+
+vagy ipythonban akár így is::
+
+    keres?
+
+"""
+
 from __future__ import print_function
+
 
 import re
 
 urls = """http://bocs.hu
     http://arek.uni-obuda.hu
     https://elearning.uni-obuda.hu
-    http://bocs.hu/ado/index.html
+    )ttp://bocs.hu/ado/index.html
     https://github.com/horvatha/linux/blob/master/regexp/keres_ipy.py
     http://.hu
     http://arek..hu
@@ -23,6 +64,7 @@ TRA-548
 KISAPA-01
 -01
 AAA-
+KISAPA-0122333
 """.split()
 
 IPs = """192.168.3.26
@@ -34,7 +76,8 @@ IPs = """192.168.3.26
     560.370.13.25
     """.split()
 
-emails = """horvath.arpad@arek.uni-obuda.hu
+emails = """
+    horvath.arpad@arek.uni-obuda.hu
     x_ipszilon@gmail.com
     """.split()
 
@@ -42,9 +85,19 @@ def keres(regexp, szavak):
     """A regexp reguláris kifejezést keres több szóban.
 
     regexp:
+        A reguláris kifejezés, amit keresünk.
         Több sorba szétszedhető (lásd re.VERBOSE).
     szavak:
         Szavak listája, amiben keres.
+
+    Példa::
+
+        keres(
+          r"^ http s? :// ([a-z-]+\.)+ [a-z]{2,3} (/[a-z-_\.]+)* $",
+          urls )
+
+    Az első paraméter érdemes r-rel kezdeni r"kifejezés" alakban, mert
+    ilyenkor nem kell levédeni a visszaper (\) karaktereket.
 
     """
     for szo in szavak:
@@ -55,3 +108,7 @@ def keres(regexp, szavak):
         else:
             print("---------")
 
+if __name__ == '__main__':
+    keres(
+      r"^ http s? :// ([a-z-]+\.)+ [a-z]{2,3} (/[a-z-_\.]+)* $",
+      urls )
--- regexp/regexp_example.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-lynx -source http://rss.freshmeat.net/freshmeat/feeds/fm-releases-global | \
-while read line; do [[ $line =~ \<title\>(.*?)\</title\> ]] && printf "%s\n" "${BASH_REMATCH[1]}"; done
